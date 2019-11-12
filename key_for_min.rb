a5657baaba993b61ed98b3239fa0d77b65dc56d8
Rose Seraphin <roseseraphin@yahoo.com>
2019-11-12 03:01:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,21 @@
 
 def key_for_min_value(name_hash)
 
-end
+ name_hash.each do |key, value| 
+ if value <= 
+   
+   return key 
+ else 
+   nil 
+
+
+
+
+end 
+
+ 
+
+
+
+
+# need to return only the smallest value 
